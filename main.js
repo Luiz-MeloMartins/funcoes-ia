7783c3d513624c55b1c24f67e7752a118436b783
Luiz-MeloMartins <107559434+Luiz-MeloMartins@users.noreply.github.com>
2024-08-23 10:44:19 -0300

    Awa

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-const caixaPrincipal = document.querySelector ("caixa-principal");
-const caixaPerguntas = document.querySelector ("caixa-perguntas");
-const caixaAlternativas= document.querySelector ("caixa-alternativas");
-const caixaResultado = document.querySelector ("caixa-resultado");
-const textoResultado = document.querySelector ("texto-resultado");
-
-
-
-
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
@@ -75,3 +71,22 @@ const perguntas = [
         ]
     },
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa.texto;
+    caixaAlternativas.appendChild(botaoAlternativas);
+}
+}
+
+mostraPergunta();
